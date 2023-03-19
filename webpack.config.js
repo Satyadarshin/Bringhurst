a852c750cf151738561e6bff85ebf18818ba9e5c
Satya Perry <s.dog@satyadarshin.com>
2023-03-19 17:02:41 +0000

    adding Webpack with Typescript and PostCSS support

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,55 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: 'style-loader',
-            },
-            {
-              loader: 'css-loader',
-              options: {
-                importLoaders: 1,
-              }
-            },
-            {
-              loader: 'postcss-loader'
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/main.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
             }
-          ]
-        }
-      ]
-    }
-  }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      },
+      {
+        test: /\.ts?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+
+            loader: 'ts-loader',
+          }
+        ],
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Template App',
+      filename: 'index.html',
+      template: 'src/templates/main.html',
+    }),
+  ]
+}
